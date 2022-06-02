c414a40af8fec820a67d958d417d3d4963882a80
0xDiSk <106767088+0xDiSk@users.noreply.github.com>
2022-06-02 19:21:05 +0200

    Update smart.sh

--- smart.sh
+++ smart.sh
@@ -1,8 +1,63 @@
 #!/bin/sh
+if [ -f /etc/redhat-release ]; then
+release="centos"
+elif cat /etc/issue | grep -Eqi "debian"; then
+release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+release="ubuntu"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+release="centos"
+elif cat /proc/version | grep -Eqi "debian"; then
+release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+release="ubuntu"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+release="centos"
+fi
+
+if  [ ! -e '/usr/bin/curl' ]; then
+echo " Installing curl ..."
+if [ "${release}" == "centos" ]; then
+yum -y install curl > /dev/null 2>&1
+else
+apt-get -y install curl > /dev/null 2>&1
+fi
+fi
+
+if  [ ! -e '/usr/bin/wget' ]; then
+echo " Installing wget ..."
+if [ "${release}" == "centos" ]; then
+yum -y install wget > /dev/null 2>&1
+else
+apt-get -y install wget > /dev/null 2>&1
+fi
+fi
+
+if  [ ! -e '/usr/sbin/smartctl' ]; then
+echo " Installing smartmontools - smartctl..."
+if [ "${release}" == "centos" ]; then
+yum -y install smartmontools > /dev/null 2>&1
+else
+apt-get -y install smartmontools > /dev/null 2>&1
+fi
+fi
+
 for disk in $(smartctl --scan|cut -d ' ' -f1) ; do
-echo "Generating S.M.A.R.T for disk: " $disk
 smart=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 smartctl -a $disk -v 1,raw24/raw32 -v 7,raw24/raw32 > $smart
+if egrep "MegaRaid" $smart
+then
+smartctl --scan|cut -d '#' -f1|cut -d "," -f2| cut -d " " -f1 > megaraid.txt
+for N in $(cat megaraid.txt) ; do
+echo "Generating S.M.A.R.T for disk (MegaRaid): " $disk,$N
+smartctl -a -v 1,raw24/raw32 -v 7,raw24/raw32 -d megaraid,$N $disk > $smart
+curl -F smart=@$smart https://disk.lol/api?disk=$disk.$N
+rm $smart
+done
+rm megaraid.txt
+else
+echo "Generating S.M.A.R.T for disk: " $disk
 curl -F smart=@$smart https://disk.lol/api?disk=$disk
+fi
 rm $smart
 done
