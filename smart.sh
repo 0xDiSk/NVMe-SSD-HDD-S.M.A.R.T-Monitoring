ef6649e7b3b961494ef460dbf5cb85b177ef195b
/dev/ <106767088+0xDiSk@users.noreply.github.com>
2022-06-03 15:13:48 +0200

    Update smart.sh

--- smart.sh
+++ smart.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 if [ -f /etc/redhat-release ]; then
 release="centos"
 elif cat /etc/issue | grep -Eqi "debian"; then
@@ -16,20 +16,46 @@ release="centos"
 fi
 
 if  [ ! -e '/usr/bin/curl' ]; then
-echo " Installing curl ..."
 if [ "${release}" == "centos" ]; then
-yum -y install curl > /dev/null 2>&1
+while true; do
+read -p "Do you wish to install curl? [Yes/No]" yn
+case $yn in
+[Yy]* ) echo " Installing curl ..."; yum -y install curl > /dev/null 2>&1; break;;
+[Nn]* ) exit;;
+* ) echo "Please answer yes or no.";;
+esac
+done
 else
-apt-get -y install curl > /dev/null 2>&1
+while true; do
+read -p "Do you wish to install this curl? [Yes/No]" yn
+case $yn in
+[Yy]* ) echo " Installing curl ..."; apt-get -y install curl > /dev/null 2>&1; break;;
+[Nn]* ) exit;;
+* ) echo "Please answer yes or no.";;
+esac
+done
 fi
 fi
 
 if  [ ! -e '/usr/sbin/smartctl' ]; then
-echo " Installing smartmontools - smartctl..."
 if [ "${release}" == "centos" ]; then
-yum -y install smartmontools > /dev/null 2>&1
+while true; do
+read -p "Do you wish to install smartmontools - smartctl? [Yes/No]" yn
+case $yn in
+[Yy]* ) echo " Installing smartmontools - smartctl ..."; yum -y install curl > /dev/null 2>&1; break;;
+[Nn]* ) exit;;
+* ) echo "Please answer yes or no.";;
+esac
+done
 else
-apt-get -y install smartmontools > /dev/null 2>&1
+while true; do
+read -p "Do you wish to install this smartmontools - smartctl? [Yes/No]" yn
+case $yn in
+[Yy]* ) echo " Installing smartmontools - smartctl ..."; apt-get -y install curl > /dev/null 2>&1; break;;
+[Nn]* ) exit;;
+* ) echo "Please answer yes or no.";;
+esac
+done
 fi
 fi
 
