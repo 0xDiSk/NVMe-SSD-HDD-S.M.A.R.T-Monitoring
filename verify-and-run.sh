7364ab8ad7844785902b1a31dc25dde485195991
/dev/ <106767088+0xDiSk@users.noreply.github.com>
2022-06-04 13:02:03 +0200

    Update verify-and-run.sh

--- verify-and-run.sh
+++ verify-and-run.sh
@@ -2,6 +2,22 @@
 hash="9731ec566206e62d945ada6365444ac0"
 disk="wget -qO- https://disk.lol/"
 
+if [ -f /etc/redhat-release ]; then
+release="centos"
+elif cat /etc/issue | grep -Eqi "debian"; then
+release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+release="ubuntu"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+release="centos"
+elif cat /proc/version | grep -Eqi "debian"; then
+release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+release="ubuntu"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+release="centos"
+fi
+
 if  [ ! -e '/usr/bin/wget' ]; then
 echo " Installing wget ..."
 if [ "${release}" == "centos" ]; then
@@ -13,7 +29,7 @@ fi
 
 if test -z "$($disk | grep -h $hash)"; then
 echo "Verification of script failed. Please update your script."
-echo "wget https://disk.lol/smart.sh"
+echo "Please download manually smart.sh script - https://disk.lol/smart.sh"
 else
 echo "Verification of script was successful. Now will run smart.sh"
 sh smart.sh
