39a8235e0eb4ba393cc440635344344eb775ac15
/dev/ <106767088+0xDiSk@users.noreply.github.com>
2022-06-09 10:27:02 +0200

    Update smartmontool-build.sh

--- smartmontool-build.sh
+++ smartmontool-build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#https://github.com/0xDiSk/NVMe-SSD-HDD-S.M.A.R.T-Monitoring/wiki/Build-smartmontools-(smartctl)-from-source.
+
 apt-get -y install automake
 git clone https://github.com/smartmontools/smartmontools.git
 cd smartmontools/smartmontools
