729358b22b7bdbc1c340679590c7d8648a9de032
/dev/ <106767088+0xDiSk@users.noreply.github.com>
2022-06-10 15:58:29 +0200

    Remove auto install of wget.
    Now when missing wget, the script will exit with [#ERROR] and [#INFO].

--- verify-and-run.sh
+++ verify-and-run.sh
@@ -1,32 +1,13 @@
 #!/bin/sh
-hash="af6dbd082d7bca2a7cfb9e7d2fce420a"
+hash="6f13a4451a197739153fd804629f60b5"
 disk="wget -qO- https://diskcheck.co/"
-hash_github="af6dbd082d7bca2a7cfb9e7d2fce420a"
+hash_github="6f13a4451a197739153fd804629f60b5"
 disk_github="wget -qO- https://raw.githubusercontent.com/0xDiSk/NVMe-SSD-HDD-S.M.A.R.T-Monitoring/main/README.md"
 
-if [ -f /etc/redhat-release ]; then
-release="centos"
-elif cat /etc/issue | grep -Eqi "debian"; then
-release="debian"
-elif cat /etc/issue | grep -Eqi "ubuntu"; then
-release="ubuntu"
-elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
-release="centos"
-elif cat /proc/version | grep -Eqi "debian"; then
-release="debian"
-elif cat /proc/version | grep -Eqi "ubuntu"; then
-release="ubuntu"
-elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
-release="centos"
-fi
-
-if  [ ! -e '/usr/bin/wget' ]; then
-echo " Installing wget ..."
-if [ "${release}" == "centos" ]; then
-yum -y install wget > /dev/null 2>&1
-else
-apt-get -y install wget > /dev/null 2>&1
-fi
+if [ ! -e "/usr/bin/wget" ]; then
+echo "[#ERROR] To verify the script hashes you will need to install wget."
+echo "[#INFO] Exiting script now."
+exit
 fi
 
 if test -z "$($disk | grep -h $hash)" || test -z "$($disk_github | grep -h $hash_github)"; then
