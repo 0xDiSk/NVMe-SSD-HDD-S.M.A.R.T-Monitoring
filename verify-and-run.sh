2713bbba0d6b5dab0cb18a0cb55cb10ab7a956c6
/dev/ <106767088+0xDiSk@users.noreply.github.com>
2023-06-10 19:28:05 +0200

    Update verify-and-run.sh

--- verify-and-run.sh
+++ verify-and-run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-hash="3d2eb2f3516bc1b3eb2ba1357de9dc29"
-disk="wget -qO- https://diskcheck.co/"
-hash_github="3d2eb2f3516bc1b3eb2ba1357de9dc29"
+hash="d300e269f1c9b6df241ac14d3d5e9954"
+disk="wget -qO- https://diskcheck.monster/"
+hash_github="d300e269f1c9b6df241ac14d3d5e9954"
 disk_github="wget -qO- https://raw.githubusercontent.com/0xDiSk/NVMe-SSD-HDD-S.M.A.R.T-Monitoring/main/README.md"
 
 if [ ! -e "/usr/bin/wget" ]; then
@@ -12,7 +12,7 @@ fi
 
 if test -z "$($disk | grep -h $hash)" || test -z "$($disk_github | grep -h $hash_github)"; then
 echo "Error: Verification of script failed - update your script."
-echo "Please download manually smart.sh script - https://diskcheck.co/smart.sh"
+echo "Please download manually smart.sh script - https://diskcheck.monster/smart.sh"
 else
 echo "Verification of script was successful."
 if [ -f "smart.sh" ]; then
@@ -20,6 +20,6 @@ echo "Now will run script smart.sh"
 sh smart.sh
 else
 echo "Error: script smart.sh missing."
-echo "Please download manually smart.sh script (file must be named as smart.sh) - https://diskcheck.co/smart.sh"
+echo "Please download manually smart.sh script (file must be named as smart.sh) - https://diskcheck.monster/smart.sh"
 fi
 fi
